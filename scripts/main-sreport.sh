092c3b75dfa526bda23526a33e3726378d6c7953
root <root@shussein.hpc.einsteinmed.edu>
2023-03-01 12:04:39 -0500

    Mahmoud changes

--- scripts/main-sreport.sh
+++ scripts/main-sreport.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 SCRIPT=$(dirname $0)
-$SCRIPT/slurm/sreport.sh|python3 $SCRIPT/slurm/parse_sreport.py
+#$SCRIPT/slurm/sreport.sh|python3 $SCRIPT/slurm/parse_sreport.py
+$SCRIPT/slurm/sreport.sh
